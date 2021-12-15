a73f0a1c5d7983e7113903ab85261451341a90fb
umnovjp <umnovjp@yahoo.com>
2021-12-14 23:27:43 -0600

    routes finally are working but still cannot run npm run seed

---
+++ routes/api.js
@@ -0,0 +1,64 @@
+const db = require("../models");
+const router = require("express").Router();
+
+// GET route
+router.get("/api/workouts", (req, res) => {
+
+    db.Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+// PUT route 
+router.put("/api/workouts/:id", (req, res) => {
+
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+});
+
+// CREATE route
+router.post("/api/workouts", ({ body }, res) => {
+ console.log(body);
+
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+// GET workouts 
+router.get("/api/workouts/range", (req, res) => {
+
+    db.Workout.find({}).then(dbWorkout => {
+        console.log("ALL WORKOUTS");
+        console.log(dbWorkout);
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+
+});
+
+module.exports = router;
---
+++ server_routesIn.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", {userNewUrlParser: true}) // added line in 10-stu
+const db = require("./models");
+// const db = mongojs(databaseUrl, collections); // to change later
+// const databaseUrl = "warmup"; // to change later
+// const collections = ["books"]; // to change later
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+db.Workout.create({ name: "New Workout" })
+  .then(dbWorkout => {
+    console.log('u r in creation')
+    console.log(dbWorkout);
+  })
+  .catch(({message}) => {
+    console.log(message);
+  });
+
+  // this part originally came from 09-stu-mongojs
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
+
+app.post("/exercise", ({ body }, res) => {
+  const exercise = body;
+
+  exercise.read = false;
+
+  db.exercises.save(book, (error, saved) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(saved);
+    }
+  });
+});
+
+app.post("/exercise?", ({ body }, res) => {
+  const exercise = body;
+
+  exercise.read = false;
+
+  db.exercises.save(book, (error, saved) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(saved);
+    }
+  });
+});
+
+app.get("/stats", (req, res) => {
+  db.exercises.find({ read: true }, (error, found) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  db.exercises.find({ read: false }, (error, found) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+//just in case if I have to create put route later
+// app.put("/markread/:id", ({ params }, res) => {
+//   db.exercsises.update(
+//     {
+//       _id: mongojs.ObjectId(params.id)
+//     },
+//     {
+//       $set: {
+//         read: true
+//       }
+//     },
+
+//     (error, edited) => {
+//       if (error) {
+//         console.log(error);
+//         res.send(error);
+//       } else {
+//         console.log(edited);
+//         res.send(edited);
+//       }
+//     }
+//   );
+// });
+
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
